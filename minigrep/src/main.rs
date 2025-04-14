e67646ab496d8ca2fd2cad9da2b5cbd220fb33cd
nyeinphyoaung <nyeinphyoaung.edu@gmail.com>
2025-04-14 19:36:49 +0630

    chore: refactoring to improve modularity and error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,20 @@
 // collect command line arguments into a vector and print them
+use minigrep::Config;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Query: {}", query);
-    println!("File path: {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should be able to read the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
